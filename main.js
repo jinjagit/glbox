0ed87ca416776d439fec948326c1c7b8079236cc
Simon Tharby <jinjamail@gmail.com>
2019-12-30 15:38:10 +0000

    prevent multiple (re)renders when mutliple resizes

--- main.js
+++ main.js
@@ -24,6 +24,20 @@ const drawLayout = () => {
   };
 };
 
+const onResize = () => {
+  const redrawIfInactive = () => {
+    let now = getTime();
+    if (now - lastResize > 995) {
+      drawLayout();
+      console.log("redraw");
+    };
+  };
+  lastResize = getTime();
+  // prevent multiple (re)renders when mutliple resizes
+  setTimeout(redrawIfInactive, 1000);
+};
 
+const getTime = typeof performance === 'function' ? performance.now : Date.now;
+let lastResize = 0;
 
 drawLayout();
