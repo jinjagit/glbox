bf510fa389cd439d332bef9c83e663d2ed7fdc34
Simon Tharby <jinjamail@gmail.com>
2019-12-31 17:37:23 +0000

    detect gpu vendor & model

--- src/glRender.js
+++ src/glRender.js
@@ -1,4 +1,12 @@
 const glRender = () => {
+  const debug = () => {
+    var debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    var vendor = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+    var renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+
+    console.log(vendor);
+    console.log(renderer);
+  };
   const canvas = document.querySelector("#glcanvas");
   // Initialize the GL context
   const gl = canvas.getContext("webgl");
@@ -11,6 +19,9 @@ const glRender = () => {
   gl.clearColor(0.2, 1.0, 0.7, 1.0);
   // Clear the color buffer with specified clear color
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  debug();
+
 };
 
 export { glRender }
